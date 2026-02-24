d857ba4d6eea109d5299d245d4f889a085c3932f
nadeem <nadeemahmad199726@gmail.com>
2026-02-24 12:56:24 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes)
 app.use("/api/auth", authRoutes)
-app.use("/api/orders",  orderRoutes)
+app.use("/api/orders", orderRoutes)
 app.use("/api/users", userRoutes)
 
 
