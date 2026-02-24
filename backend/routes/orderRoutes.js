bd389f9ac2861bd321469b43f37bfcefe80acaeb
nadeem <nadeemahmad199726@gmail.com>
2026-02-24 13:01:03 +0530

    order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,11 +2,10 @@ import express from "express";
 const router = express.Router();
 import { addOrderItems, getMyOrders, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
 
-router.post("/", addOrderItems);
-router.get("/myorders", getMyOrders)
-
-// ✅ NEW ADMIN ROUTES
 router.get("/all", getAllOrders); 
+router.get("/myorders", getMyOrders)
+router.post("/", addOrderItems);
+
 router.put("/:id/status", updateOrderStatus);
 
 export default router;
